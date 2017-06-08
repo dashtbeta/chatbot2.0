42f6494cbd6dd4122d00b3cd4f60fae14f4e1a99
Chin Yan Keat (Digi - Central) <YKCHIN@DiGiCR.digi.com.my>
2017-06-09 00:12:32 +0800

    added broadband

--- bot/index.js
+++ bot/index.js
@@ -966,11 +966,11 @@ bot.dialog('Plan-Broadband-30', [
 					 "* **10GB** for 19 Stream FREE channels\n\n" + 
 					 "* **8GB** internet quota\n\n" + 
 					 "* *Starter pack is RM38 include RM30 preload value*");
-		session.send("Would you like to \n\n" + 
-				  "* [Buy Now] (https://store.digi.com.my/storefront/product-config.ep?pID=20017&isBundle=n&ppymttype=PREPAID&ptype=BB&_ga=2.144904436.1557397186.1496910913-599147580.1493727367)\n\n" + 
-				  "* [Go to Website] (http://new.digi.com.my/broadband)\n\n" + 
-				  " or find out more?");
-		var respCards = new builder.Message(session)
+        var respCards = new builder.Message(session)
+            .text("Would you like to \n\n" + 
+				  "* [Buy Now](https://store.digi.com.my/storefront/product-config.ep?pID=20017&isBundle=n&ppymttype=PREPAID&ptype=BB&_ga=2.144904436.1557397186.1496910913-599147580.1493727367)\n\n" + 
+				  "* [Go to Website](http://new.digi.com.my/broadband)\n\n" + 
+				  "or find out more?")
             .suggestedActions(
                 builder.SuggestedActions.create(
                     session,[
@@ -992,11 +992,11 @@ bot.dialog('Plan-Broadband-60', [
 					 "* **16GB** for 19 Stream FREE channels\n\n" + 
 					 "* **8GB** for stream on demand channels\n\n" + 
 					 "* **16GB** internet quota");
-        session.send("Would you like to \n\n" + 
-				  "* [Buy Now] (https://store.digi.com.my/storefront/product-config.ep?pID=90000P&isBundle=y&ppymttype=POSTPAID&ptype=BB&_ga=2.187260008.1557397186.1496910913-599147580.1493727367)\n\n" + 
-				  "* [Go to Website] (http://new.digi.com.my/broadband)\n\n" + 
-				  " or find out more?");
         var respCards = new builder.Message(session)
+            .text("Would you like to \n\n" + 
+				  "* [Buy Now](https://store.digi.com.my/storefront/product-config.ep?pID=90000P&isBundle=y&ppymttype=POSTPAID&ptype=BB&_ga=2.187260008.1557397186.1496910913-599147580.1493727367)\n\n" + 
+				  "* [Go to Website](http://new.digi.com.my/broadband)\n\n" + 
+				  "or find out more?")
             .suggestedActions(
                 builder.SuggestedActions.create(
                     session,[
@@ -1019,11 +1019,11 @@ bot.dialog('Plan-Broadband-100', [
 					 "* **50GB** for 19 Stream FREE channels\n\n" + 
 					 "* **10GB** for stream on demand channels\n\n" + 
 					 "* **40GB** internet quota");
-        session.send("Would you like to \n\n" + 
-				  "* [Buy Now] (https://store.digi.com.my/storefront/product-config.ep?pID=90001P&isBundle=y&ppymttype=POSTPAID&ptype=BB&_ga=2.141721458.1557397186.1496910913-599147580.1493727367)\n\n" + 
-				  "* [Go to Website] (http://new.digi.com.my/broadband)\n\n" + 
-				  " or find out more?");
         var respCards = new builder.Message(session)
+            .text("Would you like to \n\n" + 
+				  "* [Buy Now](https://store.digi.com.my/storefront/product-config.ep?pID=90001P&isBundle=y&ppymttype=POSTPAID&ptype=BB&_ga=2.141721458.1557397186.1496910913-599147580.1493727367)\n\n" + 
+				  "* [Go to Website](http://new.digi.com.my/broadband)\n\n" + 
+				  "or find out more?")
             .suggestedActions(
                 builder.SuggestedActions.create(
                     session,[
